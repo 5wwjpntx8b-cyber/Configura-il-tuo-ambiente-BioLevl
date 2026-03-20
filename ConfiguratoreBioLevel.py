34fda4d0f4fd547930f0adb43dfeaf7bb606409b
5wwjpntx8b-cyber <5wwjpntx8b@privaterelay.appleid.com>
2026-03-20 22:43:38 +0100

    Update ConfiguratoreBioLevel.py

--- ConfiguratoreBioLevel.py
+++ ConfiguratoreBioLevel.py
@@ -20,8 +20,9 @@ st.markdown(
 )
 
 # Logo in alto, centrato
-st.image("cb7e327c-7c69-40e3-9b4d-d2e7557358fe.png", width=6000, use_column_width=True)
-
+st.image("cb7e327c-7c69-40e3-9b4d-d2e7557358fe.png", width=300, use_column_width=True)
+# due linee vuote
+st.markdown("<br><br>", unsafe_allow_html=True)
 # Titolo
 st.title("Configura il tuo ambiente BioLevel")
 
